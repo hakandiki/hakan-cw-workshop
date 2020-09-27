702bc4c3e556275f690d43e6b774798c580e354d
Hakan <hakandikicia@gmail.com>
2020-09-27 15:05:04 +0300

    aws\projects\001-roman-numarels-converter is added.

---
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+def not_valid(number):
+    if number.isdigit() == False:
+            print(f"Not Valid Input !!!")
+            return True
+    else:
+        numberlocal = int(number)
+        if numberlocal < 1 or numberlocal > 3999:
+            return True
+        else:
+            return False
+            
+def number_roman(number):
+    list=[]
+    if number[-1] == "4":
+        list.append("IV")
+    elif number[-1] == "9":
+        list.append("IX")
+    else:
+        list.append((((int(number[-1])//5)*"V")+(int(number[-1])%5)*"I"))
+    if len(number) > 1:
+        if number[-2] == "4":
+            list.append("IL")
+        elif number[-2] == "9":
+            list.append("IC")
+        else:
+            list.append((((int(number[-2])//5)*"L")+(int(number[-2])%5)*"X"))
+    if len(number) > 2:
+        if number[-3] == "4":
+            list.append("ID")
+        elif number[-3] == "9":
+            list.append("IM")
+        else:
+            list.append((((int(number[-3])//5)*"D")+(int(number[-3])%5)*"C"))
+    if len(number) > 3:
+        list.append(int(number[-4])*"M")
+    return("".join(list[::-1]))
+
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def head():
+    if request.method == 'POST':
+        valid = request.form['number']
+        validation = not_valid(valid)
+        if validation:
+            return render_template('index.html', not_valid = validation)
+        else:
+            result = number_roman(valid)
+            return render_template('result.html', number_decimal = valid, number_roman = result, developer_name = 'Hakan Diki')
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- python/coding-challenges/cc-001-convert-to-roman-numerals/app.py
+++ python/coding-challenges/cc-001-convert-to-roman-numerals/app.py
@@ -1,18 +1,26 @@
 #converts the given number to the Roman
-number = input("Please enter a number between 1 and 3999, inclusively: \n")
+
+
+
 while True:
-    if number.isdigit()==False:
-        print(f"Not Valid Input !!!")
-        number = input("Please enter a number between 1 and 3999, inclusively: \n")
-    else:
-        numberlocal = int(number)
-        if numberlocal < 1 or numberlocal > 3999:
+    list=[]
+    number = input("Please enter a number between 1 and 3999, inclusively: \n")    
+    while True:
+        if number.lower() == "exit":
+            break
+        if number.isdigit()==False:
             print(f"Not Valid Input !!!")
             number = input("Please enter a number between 1 and 3999, inclusively: \n")
         else:
-            break
-list=[]
-while True:
+            numberlocal = int(number)
+            if numberlocal < 1 or numberlocal > 3999:
+                print(f"Not Valid Input !!!")
+                number = input("Please enter a number between 1 and 3999, inclusively: \n")
+            else:
+                break
+    if number.lower() == "exit":
+        print("Exiting the program... Good Bye")
+        break
     if number[-1] == "4":
         list.append("IV")
     elif number[-1] == "9":
@@ -35,6 +43,5 @@ while True:
             list.append((((int(number[-3])//5)*"D")+(int(number[-3])%5)*"C"))
     if len(number) > 3:
         list.append(int(number[-4])*"M")
-    break
-print(list)
-print("".join(list[::-1]))
+    print("".join(list[::-1]))
+    
